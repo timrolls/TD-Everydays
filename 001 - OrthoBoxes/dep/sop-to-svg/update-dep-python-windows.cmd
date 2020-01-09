c09668b9a8d81c467fa4303ea343bf0f7a75370e
Tim Rolls <tim.rolls@gmail.com>
2020-01-09 11:20:35 -0800

    Day 8

---
+++ 001 - OrthoBoxes/dep/sop-to-svg/update-dep-python-windows.cmd
@@ -0,0 +1,7 @@
+:: Update dependencies
+
+:: make sure pip is up to date
+python -m pip install --user --upgrade pip
+
+:: install requirements
+pip install -r C:/Users/Research/Videos/Touchdesigner Projects/OrthoBoxes/dep/sop-to-svg/requirements.txt --target="C:/Users/Research/Videos/Touchdesigner Projects/OrthoBoxes/dep/python"
